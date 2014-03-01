72e65b1b1ea44868b5b3dc9601260269d6a3df11
discoursehosting <michael@discoursehosting.com>
2014-03-01 09:55:56 +0100

    Docs compatibility with v0.9.8.6

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
 # name: Discourse Adsense
 # about: Adds Adsense to Discourse
-# version: 0.1
+# version: 0.2
 # author: DiscourseHosting.com
-# minimum Discourse version: v0.9.8.5
+# minimum Discourse version: v0.9.8.6
 
 register_asset "javascripts/adsense.js"
 register_asset "javascripts/discourse/templates/discovery.js.handlebars"
