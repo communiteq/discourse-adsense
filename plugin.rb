ab269e1ea84c459162e457d887167c85dd5f0202
DiscourseHosting <michael@discoursehosting.com>
2014-06-11 17:05:20 +0000

    Update to Discourse 0.9.9.7

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
 # name: Discourse Adsense
 # about: Adds Adsense to Discourse
-# version: 0.7
+# version: 0.8
 # author: DiscourseHosting.com
-# minimum Discourse version: v0.9.9.3
+# minimum Discourse version: v0.9.9.7
 
 register_asset "javascripts/adsense.js"
 register_asset "javascripts/discourse/templates/discovery.js.handlebars"
