bde3bb0a43518fea3a939f6df21e632f83f90638
DiscourseHosting <michael@discoursehosting.com>
2014-07-25 22:01:23 +0000

    Changes for v0.9.9.14

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Installation
 * In production, recompile your assets: `bundle exec rake assets:precompile`
 * Restart Discourse
 
-* Minimum Discourse version: v0.9.9.13
+* Minimum Discourse version: v0.9.9.14
 
 Usage
 =====
--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
 # name: Discourse Adsense
 # about: Adds Adsense to Discourse
-# version: 0.13
+# version: 0.14
 # author: DiscourseHosting.com
-# minimum Discourse version: v0.9.9.13
+# minimum Discourse version: v0.9.9.14
 
 register_asset "javascripts/adsense.js"
 register_asset "javascripts/discourse/templates/discovery.js.handlebars"
