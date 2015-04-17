0de285d19dc6a74025aeb0b802b190e630adacdf
Neil Lalonde <neillalonde@gmail.com>
2015-04-17 11:46:20 -0400

    Use the new plugin-outlets in Discourse 1.3

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,14 @@
 # name: Discourse Adsense
 # about: Adds Adsense to Discourse
-# version: 1.2.0
+# version: 1.3.0
 # author: DiscourseHosting.com
 # url: https://www.github.com/discoursehosting/discourse-adsense
-# Supported Discourse version: v1.2
+# Supported Discourse version: v1.3
 
 register_asset "javascripts/adsense.js"
-register_asset "javascripts/discourse/templates/discovery.hbs"
-register_asset "javascripts/discourse/templates/topic.hbs"
+register_asset "javascripts/discourse/templates/connectors/discovery-list-container-top/discovery-list-container-top.hbs"
+register_asset "javascripts/discourse/templates/connectors/topic-above-post-stream/topic-above-post-stream.hbs"
+register_asset "javascripts/discourse/templates/connectors/topic-above-suggested/topic-above-suggested.hbs"
 
 register_css <<CSS
 
