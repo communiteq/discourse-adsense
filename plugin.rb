2994abdcc5bd2ae030b74652b4736cd4e56c89a6
Neil Lalonde <neillalonde@gmail.com>
2015-04-19 20:05:00 -0400

    better file names. remove deprecated use of mobileView. connector files are found automatically.

--- plugin.rb
+++ plugin.rb
@@ -6,9 +6,6 @@
 # Supported Discourse version: v1.3
 
 register_asset "javascripts/adsense.js"
-register_asset "javascripts/discourse/templates/connectors/discovery-list-container-top/discovery-list-container-top.hbs"
-register_asset "javascripts/discourse/templates/connectors/topic-above-post-stream/topic-above-post-stream.hbs"
-register_asset "javascripts/discourse/templates/connectors/topic-above-suggested/topic-above-suggested.hbs"
 
 register_css <<CSS
 
